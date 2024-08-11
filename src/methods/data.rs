40c2390563823669f72a9b59826755e34f6aaaa2
Marco Stecca <marco.stecca99@gmail.com>
2024-08-11 21:18:17 +0200

    cargo fmt

--- src/methods/data.rs
+++ src/methods/data.rs
@@ -1,9 +1,9 @@
-use sha2::{Sha256, Digest};
+use sha2::{Digest, Sha256};
 
-pub fn hash_key(key: Option<&String>) -> u64{
+pub fn hash_key(key: Option<&String>) -> u64 {
     let key = match key {
         Some(key) => key.as_str(),
-        None => ""
+        None => "",
     };
     let mut hasher = Sha256::new();
     hasher.update(key.as_bytes());
@@ -27,28 +27,34 @@ pub fn package_data(secret_data: &[u8]) -> Vec<u8> {
 }
 
 pub struct BitIterator<'a> {
-    data: &'a[u8],
+    data: &'a [u8],
     byteidx: usize,
     bitidx: usize,
 }
 
-impl <'a> BitIterator<'a> {
+impl<'a> BitIterator<'a> {
     pub fn new(data: &'a [u8]) -> Self {
-        BitIterator {data, byteidx: 0, bitidx: 7}
+        BitIterator {
+            data,
+            byteidx: 0,
+            bitidx: 7,
+        }
     }
 }
 
-impl <'a> Iterator for BitIterator<'a> {
+impl<'a> Iterator for BitIterator<'a> {
     type Item = u8;
-    
+
     fn next(&mut self) -> Option<Self::Item> {
-        if self.byteidx >= self.data.len() { return None };
+        if self.byteidx >= self.data.len() {
+            return None;
+        };
 
         let bit = (self.data[self.byteidx] >> self.bitidx) & 1;
 
         (self.byteidx, self.bitidx) = match self.bitidx {
-            0 => (self.byteidx +1 , 7),
-            _ => (self.byteidx, self.bitidx -1)
+            0 => (self.byteidx + 1, 7),
+            _ => (self.byteidx, self.bitidx - 1),
         };
         Some(bit)
     }
@@ -56,26 +62,36 @@ impl <'a> Iterator for BitIterator<'a> {
 
 pub struct BatchIterator<I> {
     iter: I,
-    size: usize
+    size: usize,
 }
 
-impl<I,T> BatchIterator<I> where I: Iterator<Item=T>,
+impl<I, T> BatchIterator<I>
+where
+    I: Iterator<Item = T>,
 {
-    pub fn new(iter:I, size:usize) -> Self {
-        BatchIterator {iter, size}
+    pub fn new(iter: I, size: usize) -> Self {
+        BatchIterator { iter, size }
     }
-
 }
 
-impl<I,T> Iterator for BatchIterator<I> where I: Iterator<Item=T>,
+impl<I, T> Iterator for BatchIterator<I>
+where
+    I: Iterator<Item = T>,
 {
     type Item = Vec<Option<T>>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let mut batch = self.iter.by_ref().take(self.size).map(Some).collect::<Vec<_>>();
-        if       batch.len() == 0 {None}
-        else if  batch.len() == self.size {Some(batch)}
-        else {
+        let mut batch = self
+            .iter
+            .by_ref()
+            .take(self.size)
+            .map(Some)
+            .collect::<Vec<_>>();
+        if batch.len() == 0 {
+            None
+        } else if batch.len() == self.size {
+            Some(batch)
+        } else {
             batch.resize_with(self.size, || None);
             Some(batch)
         }
--- src/methods/lsb.rs
+++ src/methods/lsb.rs
@@ -1,23 +1,31 @@
-use std::{collections::HashSet, path::PathBuf};
-use rand::{rngs::StdRng, Rng, SeedableRng};
 use crate::errors::AppError;
+use rand::{rngs::StdRng, Rng, SeedableRng};
+use std::{collections::HashSet, path::PathBuf};
 
-use crate::methods::data::{hash_key,package_data};
+use crate::methods::data::{hash_key, package_data};
 
 use super::data;
 
-
-pub fn embed(image_path: &PathBuf, output_path: &PathBuf, secret_data: &[u8], key: Option<&String>, verbose: bool) -> Result<(), AppError> {
+pub fn embed(
+    image_path: &PathBuf,
+    output_path: &PathBuf,
+    secret_data: &[u8],
+    key: Option<&String>,
+    verbose: bool,
+) -> Result<(), AppError> {
     let mut img = image::open(image_path)?.to_rgba8();
 
-    let (width,height) = img.dimensions();
-    let imgsize = width*height;
-    if verbose {println!("Image size: {}x{}", width, height);}
+    let (width, height) = img.dimensions();
+    let imgsize = width * height;
+    if verbose {
+        println!("Image size: {}x{}", width, height);
+    }
 
     let message_len = secret_data.len() as u32;
 
-    if message_len * 8 > (imgsize * 3) {return Err(AppError::DataOverflow)};
-
+    if message_len * 8 > (imgsize * 3) {
+        return Err(AppError::DataOverflow);
+    };
 
     let data = package_data(secret_data);
     let secret_bits = data::BitIterator::new(&data);
@@ -30,59 +38,80 @@ pub fn embed(image_path: &PathBuf, output_path: &PathBuf, secret_data: &[u8], ke
         let mut index;
         loop {
             index = rng.gen_range(0..imgsize);
-            if !used_pixels.contains(&index) {used_pixels.insert(index); break}
+            if !used_pixels.contains(&index) {
+                used_pixels.insert(index);
+                break;
+            }
         }
         let pixel = &mut img[(index % width, index / width)];
 
-        for (i,v) in trip.iter().enumerate() {
+        for (i, v) in trip.iter().enumerate() {
             pixel[i] = pixel[i] & !1 | v.unwrap_or_default();
         }
     }
 
     img.save(output_path)?;
 
-    if verbose {println!("Image saved");}
+    if verbose {
+        println!("Image saved");
+    }
 
     Ok(())
 }
 
-
-pub fn extract(image_path: &PathBuf, key: Option<&String>, verbose: bool) -> Result<Vec::<u8>, AppError> {
+pub fn extract(
+    image_path: &PathBuf,
+    key: Option<&String>,
+    verbose: bool,
+) -> Result<Vec<u8>, AppError> {
     let img = image::open(image_path)?;
     let img = img.to_rgba8();
 
-
     let mut rng = StdRng::seed_from_u64(hash_key(key));
     let mut used_pixels = HashSet::new();
-    let mut read_bits = Vec::with_capacity(33);  // minimum capacity is u32 in bits, rounded up to the nearest multiple of 3
+    let mut read_bits = Vec::with_capacity(33); // minimum capacity is u32 in bits, rounded up to the nearest multiple of 3
 
-    let (width,height) = img.dimensions();
-    let imgsize = width*height;
-    
-    for _ in 0..11{ // reads the u32 corresponding to the message size, plus one extra bit
+    let (width, height) = img.dimensions();
+    let imgsize = width * height;
+
+    for _ in 0..11 {
+        // reads the u32 corresponding to the message size, plus one extra bit
         let mut index;
         loop {
             index = rng.gen_range(0..imgsize);
-            if !used_pixels.contains(&index) {used_pixels.insert(index); break}
+            if !used_pixels.contains(&index) {
+                used_pixels.insert(index);
+                break;
+            }
         }
         let pixel = img[(index % width, index / width)];
         read_bits.push(pixel[0] & 1);
         read_bits.push(pixel[1] & 1);
         read_bits.push(pixel[2] & 1);
-    };
+    }
 
     // Uses the first 32 bits to determine the total message length, and leaves the remaining bits
-    let message_len: u32 = read_bits.drain(0..32).rev().enumerate().map(|(i, b)| (b as u32) << i).sum();
-    if verbose {println!("Detected message length: {}", message_len)};
-    
+    let message_len: u32 = read_bits
+        .drain(0..32)
+        .rev()
+        .enumerate()
+        .map(|(i, b)| (b as u32) << i)
+        .sum();
+    if verbose {
+        println!("Detected message length: {}", message_len)
+    };
+
     let mut secret = Vec::<u8>::with_capacity(message_len as usize);
 
     // Keeps reading bits and storing into read_bits, when there are enough for a byte one is created and pushed into secret
-    for _ in 0..(message_len * 8 -1).div_ceil(3){
+    for _ in 0..(message_len * 8 - 1).div_ceil(3) {
         let mut index;
         loop {
             index = rng.gen_range(0..imgsize);
-            if !used_pixels.contains(&index) {used_pixels.insert(index); break}
+            if !used_pixels.contains(&index) {
+                used_pixels.insert(index);
+                break;
+            }
         }
         let pixel = img[(index % width, index / width)];
         read_bits.push(pixel[0] & 1);
@@ -90,12 +119,20 @@ pub fn extract(image_path: &PathBuf, key: Option<&String>, verbose: bool) -> Res
         read_bits.push(pixel[2] & 1);
 
         if read_bits.len() >= 8 {
-            secret.push(read_bits.drain(0..8).rev().enumerate().map(|(i,b)| b<<i).sum());
+            secret.push(
+                read_bits
+                    .drain(0..8)
+                    .rev()
+                    .enumerate()
+                    .map(|(i, b)| b << i)
+                    .sum(),
+            );
         }
     }
-    
-    if verbose {println!("Message length: {}", message_len)};
+
+    if verbose {
+        println!("Message length: {}", message_len)
+    };
 
     Ok(secret)
-
 }
