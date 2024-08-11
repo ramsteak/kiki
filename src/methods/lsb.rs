f1b83f33c3832293221b4db8ed08b6718093e921
Marco Stecca <marco.stecca99@gmail.com>
2024-08-11 21:18:02 +0200

    Refactor using custom data iterators

--- src/methods/lsb.rs
+++ src/methods/lsb.rs
@@ -8,10 +8,8 @@ use super::data;
 
 
 pub fn embed(image_path: &PathBuf, output_path: &PathBuf, secret_data: &[u8], key: Option<&String>, verbose: bool) -> Result<(), AppError> {
-    let img = image::open(image_path)?;
+    let mut img = image::open(image_path)?.to_rgba8();
 
-    let mut img = img.to_rgba8();
-    
     let (width,height) = img.dimensions();
     let imgsize = width*height;
     if verbose {println!("Image size: {}x{}", width, height);}
@@ -20,20 +18,15 @@ pub fn embed(image_path: &PathBuf, output_path: &PathBuf, secret_data: &[u8], ke
 
     if message_len * 8 > (imgsize * 3) {return Err(AppError::DataOverflow)};
 
-    let data = package_data(secret_data);
-    let mut secret_bits = data::BitIterator::new(&data);
 
-    let mut get_next_trip = || -> Option<(u8,u8,u8)> {
-        let r = secret_bits.next()?;
-        let g = secret_bits.next().unwrap_or(0);
-        let b = secret_bits.next().unwrap_or(0);
-        Some((r,g,b))
-    };
+    let data = package_data(secret_data);
+    let secret_bits = data::BitIterator::new(&data);
+    let bit_triplet = data::BatchIterator::new(secret_bits, 3);
 
     let mut rng = StdRng::seed_from_u64(hash_key(key));
     let mut used_pixels = HashSet::new();
 
-    for _ in 0..(data.len()*8).div_ceil(3){
+    for trip in bit_triplet {
         let mut index;
         loop {
             index = rng.gen_range(0..imgsize);
@@ -41,13 +34,8 @@ pub fn embed(image_path: &PathBuf, output_path: &PathBuf, secret_data: &[u8], ke
         }
         let pixel = &mut img[(index % width, index / width)];
 
-        match get_next_trip() {
-            Some((r,g,b)) => {
-                pixel[0] = pixel[0] & !1 | r;
-                pixel[1] = pixel[1] & !1 | g;
-                pixel[2] = pixel[2] & !1 | b;
-            },
-            None => break,
+        for (i,v) in trip.iter().enumerate() {
+            pixel[i] = pixel[i] & !1 | v.unwrap_or_default();
         }
     }
 
